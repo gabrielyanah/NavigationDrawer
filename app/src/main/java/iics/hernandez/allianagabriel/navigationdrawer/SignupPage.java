05c705565f15057723feb3edbe0e5e366c51d0d8
flavian <2015086506@ust-ics.mygbiz.com>
2018-11-17 09:08:00 +0800

    Initial commit

---
+++ app/src/main/java/iics/hernandez/allianagabriel/navigationdrawer/SignupPage.java
@@ -0,0 +1,13 @@
+package iics.hernandez.allianagabriel.navigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+}
